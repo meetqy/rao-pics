3e6ecaba76c9212af4fe7a300a205b28445fb209
meetqy <xiaoqiyang@xique.com>
2022-12-15 19:01:44 +0800

    chore: configProvider 测试

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,15 +1,29 @@
-import { ConfigProvider, Layout } from "antd";
+import { Button, ConfigProvider, Layout, theme, Typography } from "antd";
 import zhCN from "antd/locale/zh_CN";
 import "antd/dist/reset.css";
 import styles from "./index.module.css";
 
 export const MyLayout = ({ children }) => {
   return (
-    <ConfigProvider locale={zhCN}>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#333",
+        },
+        algorithm: theme.darkAlgorithm,
+      }}
+      locale={zhCN}
+    >
       <Layout className={styles.wrapper}>
-        <Layout.Sider>left sider</Layout.Sider>
+        <Layout.Sider width={240} theme="light">
+          <Typography.Text>Left sider</Typography.Text>
+          <Button type="primary">primary</Button>
+        </Layout.Sider>
         <Layout.Content>{children}</Layout.Content>
-        <Layout.Sider>right sider</Layout.Sider>
+        <Layout.Sider width={240} theme="light">
+          <Typography.Text>right sider</Typography.Text>
+          <Button>123</Button>
+        </Layout.Sider>
       </Layout>
     </ConfigProvider>
   );
