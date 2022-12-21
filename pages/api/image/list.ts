1358d5b000b971494f3e5af2f0051f5f57f5a2ed
meetqy <xiaoqiyang@xique.com>
2022-12-21 16:47:47 +0800

    chore: 删除swagger注释

--- pages/api/image/list.ts
+++ pages/api/image/list.ts
@@ -1,29 +1,5 @@
 import prisma from "@/lib/prisma";
 
-/**
- * @openapi
- * /api/image/list:
- *   post:
- *     tags:
- *       - Image
- *     summary: 获取图片列表
- *     parameters:
- *       - name: page
- *         in: query
- *         description: page
- *         schema:
- *            type: int
- *            default: 0
- *       - name: pageSize
- *         in: query
- *         description: pageSize
- *         schema:
- *           type: int
- *           default: 50
- *     responses:
- *       200:
- *         description: Returns Object
- */
 export default async function handler(req, res) {
   // findMany参考：https://www.prisma.io/docs/reference/api-reference/prisma-client-reference?query=t&page=1#findmany
 
--- pages/api/tag/index.ts
+++ pages/api/tag/index.ts
@@ -1,16 +1,5 @@
 import prisma from "@/lib/prisma";
 
-/**
- * @openapi
- * /api/tag:
- *   get:
- *     tags:
- *        - Tag
- *     summary: 获取所有标签信息
- *     responses:
- *       200:
- *         description: Returns Object
- */
 export default async function handler(_req, res) {
   const [count, data] = await Promise.all([
     prisma.tag.count(),
