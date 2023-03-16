160c5b902c36449887c450592277afcc1e09440a
meetqy <meetqy@qq.com>
2023-03-16 14:39:30 +0800

    feat: 🎸 eagleuse 新增API transform参数控制

--- packages/eagleuse/lib/index.ts
+++ packages/eagleuse/lib/index.ts
@@ -2,23 +2,35 @@ import PLUGIN_API from "@eagleuse/plugin-api";
 import dotenv from "dotenv-flow";
 import TransformEagle from "@eagleuse/transform-eagle";
 
-const EagleUse = () => {
+interface Args {
+  // 开启转换 eagle,默认 true
+  transform_eagle: boolean;
+  // 开启sqlite api访问, 默认 true
+  plugin_api: boolean;
+}
+
+const EagleUse = (args: Args) => {
   const { LIBRARY, DATABASE_URL, NSFW = "false", PORT = 3002 } = process.env;
+  const { transform_eagle = true, plugin_api = true } = args;
 
   if (!LIBRARY) throw Error("LIBRARY is null!");
   if (!DATABASE_URL) throw Error("DATABASE_URL is null!");
 
   dotenv.config();
 
-  TransformEagle({
-    library: LIBRARY,
-    plugin_nsfw: NSFW === "true",
-  });
+  if (transform_eagle) {
+    TransformEagle({
+      library: LIBRARY,
+      plugin_nsfw: NSFW === "true",
+    });
+  }
 
-  PLUGIN_API({
-    library: LIBRARY,
-    port: +PORT,
-  });
+  if (plugin_api) {
+    PLUGIN_API({
+      library: LIBRARY,
+      port: +PORT,
+    });
+  }
 };
 
 export default EagleUse;
