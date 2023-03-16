00e18100335ee48c7215d836a336fc7df0b4c9d4
meetqy <meetqy@qq.com>
2023-03-16 16:31:09 +0800

    fix: 🐛 参数兼容

--- packages/eagleuse/lib/index.ts
+++ packages/eagleuse/lib/index.ts
@@ -4,14 +4,14 @@ import TransformEagle from "@eagleuse/transform-eagle";
 
 interface Args {
   // 开启转换 eagle,默认 true
-  transform_eagle: boolean;
+  transform_eagle?: boolean;
   // 开启sqlite api访问, 默认 true
-  plugin_api: boolean;
+  plugin_api?: boolean;
 }
 
 const EagleUse = (args: Args) => {
   const { LIBRARY, DATABASE_URL, NSFW = "false", PORT = 3002 } = process.env;
-  const { transform_eagle = true, plugin_api = true } = args;
+  const { transform_eagle = true, plugin_api = true } = args || {};
 
   if (!LIBRARY) throw Error("LIBRARY is null!");
   if (!DATABASE_URL) throw Error("DATABASE_URL is null!");
