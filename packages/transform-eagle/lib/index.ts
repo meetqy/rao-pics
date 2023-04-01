d7b1000b3c823bf16c3c74d0185178b19c56472a
meetqy <meetqy@qq.com>
2023-04-01 15:27:18 +0800

    refactor: 💡 移除plugin_nsfw

--- packages/transform-eagle/lib/index.ts
+++ packages/transform-eagle/lib/index.ts
@@ -1,25 +1,21 @@
-import watchImage from "./image";
+import watchImage, { Transform as _Transfrom } from "./image";
 import watchMetadata from "./metadata";
 import { logger } from "@raopics/utils";
-import { getNSFW } from "./image/nsfw";
 import watchStarredTags from "./starred-tags";
 import { getPrisma } from "@raopics/prisma-client";
 
-interface Args {
+export * from "./types";
+export type Transform = _Transfrom;
+
+export interface Args {
   library: string;
-  plugin_nsfw?: boolean;
+  transform: _Transfrom;
 }
 
 const TransformEagle = async (args: Args) => {
-  const { library, plugin_nsfw = false } = args;
+  const { library, transform } = args;
 
   process.env.LIBRARY = library;
-  process.env.PLUGIN_NSFW = String(plugin_nsfw);
-
-  if (plugin_nsfw) {
-    await getNSFW();
-    logger.info("Init nsfw success.");
-  }
 
   logger.info("Start transform 🛫");
 
@@ -30,8 +26,8 @@ const TransformEagle = async (args: Args) => {
 
   // 延迟一个 wait 时间
   setTimeout(() => {
-    watchImage(library);
-  }, 5000);
+    watchImage(library, transform);
+  }, 3000);
 };
 
 export default TransformEagle;
