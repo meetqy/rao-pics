2f6f21b88d56d46f401d3d255b203c6f78fcfeaf
meetqy <meetqy@qq.com>
2023-04-11 13:49:59 +0800

    fix: 🐛 #127

--- packages/prisma-client/lib/index.ts
+++ packages/prisma-client/lib/index.ts
@@ -33,14 +33,13 @@ export const getPrisma = (library?: string) => {
   copySync(join(__dirname, "../prisma/default.db"), dbUrl, { overwrite: false });
 
   if (!watchDBFile) {
+    watchDBFile = true;
     chokidar
       .watch(dbUrl)
       .on("all", updatePrismaClient)
       .on("ready", () => {
         logger.info(`[prisma-client] start watching ${dbUrl}`);
       });
-
-    watchDBFile = true;
   }
 
   if (!prisma) {
