42b29b4ffcf3b60e32eb934802742a299a659c06
meetqy <meetqy@qq.com>
2023-04-11 15:39:44 +0800

    perf: ⚡️ prisma-client 触发更新prisma方式改为 change

--- packages/prisma-client/lib/index.ts
+++ packages/prisma-client/lib/index.ts
@@ -36,7 +36,11 @@ export const getPrisma = (library?: string) => {
     watchDBFile = true;
     chokidar
       .watch(dbUrl)
-      .on("all", updatePrismaClient)
+      .on("all", (event) => {
+        if (event === "change") {
+          updatePrismaClient();
+        }
+      })
       .on("ready", () => {
         logger.info(`[prisma-client] start watching ${dbUrl}`);
       });
