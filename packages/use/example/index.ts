cb2118982fd0d70cd6897e35115d77165e4a9dec
meetqy <meetqy@qq.com>
2023-04-28 09:54:33 +0800

    feat: 🎸 nsfw model 支持配置

--- packages/use/example/index.ts
+++ packages/use/example/index.ts
@@ -6,7 +6,10 @@ raopics({
   library: "/Users/qymeet/Pictures/test.library",
   transform: {
     before: async (args) => {
-      const metadata = await PLUGIN_NSFW(args, 0.35);
+      const metadata = await PLUGIN_NSFW(args, {
+        model: "model",
+        modelOptions: { size: 299 },
+      });
       return metadata;
     },
   },
