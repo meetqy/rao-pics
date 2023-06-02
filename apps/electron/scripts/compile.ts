c857a22bffe88b02ad4ec25d901d00c40cd7d4e1
meetqy <meetqy@qq.com>
2023-06-02 15:41:13 +0800

    perf: ⚡️ 打包时，根据平台添加额外的prisma可执行文件
    dmg 体积减少8M
    
    close #158

--- apps/electron/scripts/compile.ts
+++ apps/electron/scripts/compile.ts
@@ -1,48 +1,58 @@
 import builder from "electron-builder";
 
+const extraResources: builder.FileSet[] = [];
+
+export const Config: builder.Configuration = {
+  productName: "Rao Pics",
+  copyright: `Copyright © 2022-${new Date().getFullYear()} meetqy`,
+  mac: {
+    category: "public.app-category.photography",
+    icon: "buildResources/icon.icns",
+    darkModeSupport: true,
+    target: {
+      target: "dmg",
+      arch: ["x64", "arm64"],
+    },
+    extraResources: extraResources,
+  },
+  extraMetadata: { version: "0.5.0-beta.2" },
+  directories: {
+    output: "dist",
+    buildResources: "buildResources",
+  },
+  files: ["main/dist/**", "preload/dist/**", "renderer/dist/**"],
+  extraResources: [
+    {
+      from: "../../packages/db/prisma/db.sqlite",
+      to: "packages/db/prisma/db.sqlite",
+    },
+    {
+      from: "../nextjs/.next/static",
+      to: "apps/nextjs/.next/static",
+      filter: ["**/*"],
+    },
+    {
+      from: "../nextjs/public",
+      to: "apps/nextjs/public",
+      filter: ["**/*"],
+    },
+  ],
+  beforeBuild: async (context) => {
+    console.log(context.platform.nodeName, context.arch);
+
+    extraResources.pop();
+    extraResources.push({
+      from: "../nextjs/.next/standalone",
+      filter: ["**/*", "!**/.prisma/client/*.node", `**/.prisma/client/*darwin${context.arch === "x64" ? "" : "-arm64"}.*.node`],
+    });
+
+    return Promise.resolve(context);
+  },
+};
+
 builder
   .build({
-    targets: builder.Platform.MAC.createTarget(),
-    config: {
-      productName: "Rao Pics",
-      copyright: `Copyright © 2022-${new Date().getFullYear()} meetqy`,
-      asar: false,
-      mac: {
-        category: "public.app-category.photography",
-        icon: "buildResources/icon.icns",
-        darkModeSupport: true,
-        target: {
-          target: "dmg",
-          arch: ["arm64", "x64"],
-        },
-      },
-      extraMetadata: { version: "0.5.0-beta.2" },
-      directories: {
-        output: "dist",
-        buildResources: "buildResources",
-      },
-      files: ["main/dist/**", "preload/dist/**", "renderer/dist/**"],
-      extraResources: [
-        {
-          from: "../../packages/db/prisma/db.sqlite",
-          to: "packages/db/prisma/db.sqlite",
-        },
-        {
-          from: "../nextjs/.next/standalone",
-          filter: ["**/*"],
-        },
-        {
-          from: "../nextjs/.next/static",
-          to: "apps/nextjs/.next/static",
-          filter: ["**/*"],
-        },
-        {
-          from: "../nextjs/public",
-          to: "apps/nextjs/public",
-          filter: ["**/*"],
-        },
-      ],
-    },
+    config: Config,
   })
   .then((result) => {
     console.log(JSON.stringify(result));
