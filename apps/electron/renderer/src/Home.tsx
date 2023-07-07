93e5a671986df2b02270e14f713e02e36403545e
meetqy <meetqy@qq.com>
2023-07-07 11:21:19 +0800

    perf: ⚡️ 优化控制台文案

--- apps/electron/renderer/src/Home.tsx
+++ apps/electron/renderer/src/Home.tsx
@@ -240,11 +240,22 @@ function Home() {
                   />
                 </svg>
 
-                <span className="ml-2">已同步/失败</span>
+                <div className="ml-2 flex items-center">
+                  已同步/未同步
+                  <div className="tooltip before:w-max" data-tip="未同步：已放入回收站、读取失败等素材">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-base-content/50">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                      />
+                    </svg>
+                  </div>
+                </div>
               </span>
-              <span className=" font-medium font-mono">
-                <span className=" text-primary">{activeItem?._count.images}</span>
-                <span className=" font-normal text-base-300 mx-2">|</span>
+              <span className="font-medium font-mono">
+                <span className="text-success">{activeItem?._count.images}</span>
+                <span className="font-extralight text-base-content/50 mx-2 relative -top-0.5">|</span>
                 <span className="text-error">{activeItem?._count.fails}</span>
               </span>
             </div>
