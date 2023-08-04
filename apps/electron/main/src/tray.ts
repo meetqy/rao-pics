2f767124e5637efc267302cc820d4245cacffd21
meetqy <meetqy@icloud.com>
2023-08-04 11:28:15 +0800

    fix: 🐛 mac opt+track don't show menu
    ✅ Closes: #363

--- apps/electron/main/src/tray.ts
+++ apps/electron/main/src/tray.ts
@@ -16,61 +16,63 @@ const createTray = () => {
   // 托盘图标
   const tray = new Tray(getIcon("tray"));
 
-  const getMenu = () =>
-    Menu.buildFromTemplate([
-      { type: "separator" },
-      {
-        label: "下载页面",
-        type: "normal",
-        icon: getIcon("download"),
-        click: () => {
-          void shell.openExternal("https://github.com/rao-pics/core/releases");
-        },
+  const contextMenu = Menu.buildFromTemplate([
+    { type: "separator" },
+    {
+      label: "下载页面",
+      type: "normal",
+      icon: getIcon("download"),
+      click: () => {
+        void shell.openExternal("https://github.com/rao-pics/core/releases");
       },
-      {
-        label: "最新动态",
-        icon: getIcon("twitter"),
-        type: "normal",
-        click: () => {
-          void shell.openExternal("https://twitter.com/meetqy");
-        },
+    },
+    {
+      label: "最新动态",
+      icon: getIcon("twitter"),
+      type: "normal",
+      click: () => {
+        void shell.openExternal("https://twitter.com/meetqy");
       },
-      {
-        label: "开发进度",
-        icon: getIcon("todo"),
-        type: "normal",
-        click: () => {
-          void shell.openExternal("https://github.com/orgs/rao-pics/projects/1/views/1");
-        },
+    },
+    {
+      label: "开发进度",
+      icon: getIcon("todo"),
+      type: "normal",
+      click: () => {
+        void shell.openExternal("https://github.com/orgs/rao-pics/projects/1/views/1");
       },
-      { type: "separator" },
-      {
-        label: "退出",
-        type: "normal",
-        click: () => {
-          globalApp.isQuite = true;
-          app.quit();
-        },
+    },
+    { type: "separator" },
+    {
+      label: "退出",
+      type: "normal",
+      click: () => {
+        globalApp.isQuite = true;
+        app.quit();
       },
-    ]);
+    },
+  ]);
 
-  tray.on("click", () => {
-    void restoreOrCreateWindow().then((window) => {
-      window.show();
-      window.focus();
-    });
+  tray.on("click", (e) => {
+    if (e.altKey) {
+      tray.popUpContextMenu(contextMenu);
+    } else {
+      void restoreOrCreateWindow().then((window) => {
+        window.show();
+        window.focus();
+      });
+    }
   });
+
   tray.on("right-click", () => {
-    tray.popUpContextMenu(getMenu());
+    tray.popUpContextMenu(contextMenu);
   });
 
   /** 监听 dark/light */
   nativeTheme.on("updated", () => {
     tray.setImage(getIcon("tray"));
-    tray.setContextMenu(getMenu());
+    tray.setContextMenu(contextMenu);
   });
-
-  return tray;
 };
 
 export default createTray;
