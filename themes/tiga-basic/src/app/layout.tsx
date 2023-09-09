da52f98ccf62d94c51f602da8cf4e7b525589fed
meetqy <meetqy@qq.com>
2023-09-09 16:05:31 +0800

    server

--- themes/tiga-basic/src/app/layout.tsx
+++ themes/tiga-basic/src/app/layout.tsx
@@ -2,6 +2,9 @@ import "~/styles/globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { headers } from "next/headers";
+
+import { TRPCReactProvider } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+      </body>
     </html>
   );
 }
