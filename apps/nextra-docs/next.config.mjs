decf583ecb462739f03195b0a77f8c6a293d3d3d
meetqy <meetqy@qq.com>
2023-09-17 11:05:41 +0800

    extra-docs default template

---
+++ apps/nextra-docs/next.config.mjs
@@ -0,0 +1,12 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+export default withNextra({
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+});
