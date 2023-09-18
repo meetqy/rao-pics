eb409a3e55faabefdef939ad66d604d9e5f56e61
meetqy <meetqy@qq.com>
2023-09-18 11:39:59 +0800

    pwa

--- themes/tiga-basic/src/pages/_app.tsx
+++ themes/tiga-basic/src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
 import { trpc } from "~/utils/trpc";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default trpc.withTRPC(App);
