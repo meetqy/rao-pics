0d455815187906672f9982e7b4ad7a1f2151b506
meetqy <meetqy@qq.com>
2023-09-19 15:36:16 +0800

    sentry debug

--- apps/electron/src/main/index.ts
+++ apps/electron/src/main/index.ts
@@ -20,7 +20,7 @@ import { createCustomIPCHandle } from "./src/ipc";
  */
 Sentry.init({
   dsn: "https://178a415c4ef2421a8f52b6c4041319af@o4505321607397376.ingest.sentry.io/4505321612705792",
-  debug: false,
+  debug: IS_DEV,
 });
 
 const controller = new AbortController();
