ac927f6280052b081fa0ccff63746b8b5cf906e6
meetqy <meetqy@qq.com>
2023-10-05 14:53:57 +0800

    fix: 【Electron】根据平台打包 Prisma 二进制文件 #435

---
+++ apps/electron/electron-builder/win.cjs
@@ -0,0 +1,42 @@
+const BaseConfig = require("./base.cjs");
+const builder = require("electron-builder");
+const { join } = require("path");
+
+const { Platform } = builder;
+
+const isTestBuilder = process.env.IS_TEST_BUILDER === "true";
+
+/**
+ * @type {import('electron-builder').Configuration}
+ * @see https://www.electron.build/configuration/configuration
+ */
+const AppConfig = {
+  ...BaseConfig,
+
+  win: {
+    icon: "resources/icon.png",
+    target: {
+      target: isTestBuilder ? "dir" : "nsis",
+      arch: ["x64"],
+    },
+    files: [
+      {
+        from: join(__dirname, "../../../node_modules/.prisma"),
+        to: "node_modules/.prisma",
+        filter: ["**/*", "!**/*.node", "**/query_engine-windows.dll.node"],
+      },
+    ],
+  },
+};
+
+builder
+  .build({
+    targets: Platform.WINDOWS.createTarget(),
+    config: AppConfig,
+  })
+  .then((result) => {
+    console.log(JSON.stringify(result));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
