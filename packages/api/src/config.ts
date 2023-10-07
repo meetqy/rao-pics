7932f258f8a1a378535b108b7303df2331880890
meetqy <meetqy@qq.com>
2023-10-07 17:52:33 +0800

    config 重构

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -5,48 +5,55 @@ import { prisma } from "@rao-pics/db";
 
 import { t } from "./utils";
 
+export const configInput = {
+  upsert: z.object({
+    language: z.enum(["zh-cn", "en-us", "zh-tw"]).optional(),
+    theme: z.string().optional(),
+    color: z.string().optional(),
+    serverPort: z.number().optional(),
+    clientPort: z.number().optional(),
+    ip: z.string().optional(),
+    pwdFolder: z.boolean().optional(),
+    trash: z.boolean().optional(),
+  }),
+};
+
+export const configCore = {
+  findUnique: async () =>
+    await prisma.config.findFirst({ where: { name: "config" } }),
+
+  upsert: async (input: z.infer<typeof configInput.upsert>) => {
+    return await prisma.config.upsert({
+      where: { name: "config" },
+      update: {
+        language: input.language ?? undefined,
+        color: input.color ?? undefined,
+        theme: input.theme ?? undefined,
+        ip: input.ip ?? undefined,
+        serverPort: input.serverPort ?? undefined,
+        clientPort: input.clientPort ?? undefined,
+        trash: input.trash ?? undefined,
+        pwdFolder: input.pwdFolder ?? undefined,
+      },
+      create: {
+        name: "config",
+        language: input.language ?? "zh-cn",
+        color: input.color ?? "light",
+        theme: input.theme ?? DEFAULT_THEME,
+        ip: input.ip,
+        serverPort: input.serverPort,
+        clientPort: input.clientPort,
+        trash: input.trash ?? false,
+        pwdFolder: input.pwdFolder ?? false,
+      },
+    });
+  },
+};
+
 export const config = t.router({
   upsert: t.procedure
-    .input(
-      z.object({
-        language: z.enum(["zh-cn", "en-us", "zh-tw"]).optional(),
-        theme: z.string().optional(),
-        color: z.string().optional(),
-        serverPort: z.number().optional(),
-        clientPort: z.number().optional(),
-        ip: z.string().optional(),
-        pwdFolder: z.boolean().optional(),
-        trash: z.boolean().optional(),
-      }),
-    )
-    .mutation(async ({ input }) => {
-      return await prisma.config.upsert({
-        where: { name: "config" },
-        update: {
-          language: input.language ?? undefined,
-          color: input.color ?? undefined,
-          theme: input.theme ?? undefined,
-          ip: input.ip ?? undefined,
-          serverPort: input.serverPort ?? undefined,
-          clientPort: input.clientPort ?? undefined,
-          trash: input.trash ?? undefined,
-          pwdFolder: input.pwdFolder ?? undefined,
-        },
-        create: {
-          name: "config",
-          language: input.language ?? "zh-cn",
-          color: input.color ?? "light",
-          theme: input.theme ?? DEFAULT_THEME,
-          ip: input.ip,
-          serverPort: input.serverPort,
-          clientPort: input.clientPort,
-          trash: input.trash ?? false,
-          pwdFolder: input.pwdFolder ?? false,
-        },
-      });
-    }),
+    .input(configInput.upsert)
+    .mutation(({ input }) => configCore.upsert(input)),
 
-  findUnique: t.procedure.query(async () => {
-    return await prisma.config.findFirst({ where: { name: "config" } });
-  }),
+  findUnique: t.procedure.query(configCore.findUnique),
 });
