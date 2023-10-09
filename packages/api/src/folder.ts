cb9893ddd2533918ff6ab9b30cc02a2ee5297567
meetqy <meetqy@qq.com>
2023-10-09 15:26:28 +0800

    change folder.show

--- packages/api/src/folder.ts
+++ packages/api/src/folder.ts
@@ -4,6 +4,33 @@ import { prisma } from "@rao-pics/db";
 
 import { t } from "./utils";
 
+export const folderInput = {
+  find: z
+    .object({
+      id: z.string().optional(),
+      pid: z.string().optional(),
+    })
+    .optional(),
+};
+
+export const folderCore = {
+  find: async (input: z.infer<typeof folderInput.find>) => {
+    if (input?.id) {
+      return await prisma.folder.findUnique({
+        where: { id: input.id },
+      });
+    }
+
+    if (input?.pid) {
+      return await prisma.folder.findMany({
+        where: { pid: input.pid },
+      });
+    }
+
+    return await prisma.folder.findMany();
+  },
+};
+
 export const folder = t.router({
   upsert: t.procedure
     .input(
@@ -14,10 +41,32 @@ export const folder = t.router({
         pid: z.string().optional(),
         password: z.string().optional(),
         passwordTips: z.string().optional(),
-        show: z.boolean().optional(),
+        show: z.boolean().optional().default(true),
       }),
     )
     .mutation(async ({ input }) => {
+      const { password, show } = input;
+
+      if (password) {
+        input.show = false;
+      }
+
+      if (show) {
+        // 更新 show, 需要同步更新子文件夹中的 show
+        await prisma.folder.updateMany({
+          where: {
+            OR: [
+              // 父级在 upsert 中更新
+              // { id: input.id },
+              { pid: input.id },
+            ],
+          },
+          data: {
+            show: input.show,
+          },
+        });
+      }
+
       return await prisma.folder.upsert({
         where: { id: input.id },
         create: input,
@@ -26,27 +75,6 @@ export const folder = t.router({
     }),
 
   find: t.procedure
-    .input(
-      z
-        .object({
-          id: z.string().optional(),
-          pid: z.string().optional(),
-        })
-        .optional(),
-    )
-    .query(async ({ input }) => {
-      if (input?.id) {
-        return await prisma.folder.findUnique({
-          where: { id: input.id },
-        });
-      }
-
-      if (input?.pid) {
-        return await prisma.folder.findMany({
-          where: { pid: input.pid },
-        });
-      }
-
-      return await prisma.folder.findMany();
-    }),
+    .input(folderInput.find)
+    .query(async ({ input }) => folderCore.find(input)),
 });
