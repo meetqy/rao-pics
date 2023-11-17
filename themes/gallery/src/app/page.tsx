464f52860b9e5ab9f481331a53d87d52fd0e54dd
meetqy <meetqy@qq.com>
2023-11-17 13:22:42 +0800

    一些小问题

--- themes/gallery/src/app/page.tsx
+++ themes/gallery/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useRecoilValue } from "recoil";
 
 import { settingSelector } from "~/states/setting";
