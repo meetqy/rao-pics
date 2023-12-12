83d7ac5316a174877f12aea31b77ba5c7a940d05
meetqy <meetqy@qq.com>
2023-12-12 11:02:11 +0800

    👌 IMPROVE: 文件夹无素材，展示占位符

--- themes/gallery/src/app/_components/ChildFolderCardList.tsx
+++ themes/gallery/src/app/_components/ChildFolderCardList.tsx
@@ -6,6 +6,7 @@ import { useRecoilValue } from "recoil";
 
 import { settingSelector } from "~/states/setting";
 import { trpc } from "~/utils/trpc";
+import Error from "./Error";
 
 const ChildFolderCardList = ({ folderId }: { folderId: string }) => {
   const router = useRouter();
@@ -22,64 +23,68 @@ const ChildFolderCardList = ({ folderId }: { folderId: string }) => {
 
   const children = folder?.children ?? [];
 
-  return (
-    children.length > 0 && (
-      <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
-        {children.map((child) => {
-          const Card = (props: { children?: ReactNode }) => {
-            return (
-              <div
-                onClick={() => {
-                  void router.push(`/${layout}?m=${child.id}`);
-                }}
-                aria-hidden
-                key={child.id}
-                className="group relative flex aspect-square w-full cursor-pointer items-end justify-center overflow-hidden rounded-md border border-base-content/10 bg-base-200/30 p-2"
-              >
-                <div className="relative z-10 w-full overflow-hidden rounded-md bg-base-100/70 py-2 text-center backdrop-blur-sm">
-                  <p className="text-base font-medium">{child.name}</p>
-                  <p className="mt-1 text-xs text-base-content/70">
-                    {child._count.images
-                      ? `${child._count.images} 个文件`
-                      : "查看子文件夹"}
-                  </p>
-                </div>
-
-                {props.children}
+  return children.length > 0 ? (
+    <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
+      {children.map((child) => {
+        const Card = (props: { children?: ReactNode }) => {
+          return (
+            <div
+              onClick={() => {
+                void router.push(`/${layout}?m=${child.id}`);
+              }}
+              aria-hidden
+              key={child.id}
+              className="group relative flex aspect-square w-full cursor-pointer items-end justify-center overflow-hidden rounded-md border border-base-content/10 bg-base-200/30 p-2"
+            >
+              <div className="relative z-10 w-full overflow-hidden rounded-md bg-base-100/70 py-2 text-center backdrop-blur-sm">
+                <p className="text-base font-medium">{child.name}</p>
+                <p className="mt-1 text-xs text-base-content/70">
+                  {child._count.images
+                    ? `${child._count.images} 个文件`
+                    : "查看子文件夹"}
+                </p>
               </div>
-            );
-          };
 
-          const image = child.images?.[0];
-          if (!image || !config) {
-            return (
-              <Card key={child.id}>
-                <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-neutral pb-14 transition-all ease-in-out">
-                  <LightBulbIcon className="h-24 w-24 text-neutral-content" />
-                </div>
-              </Card>
-            );
-          }
+              {props.children}
+            </div>
+          );
+        };
 
-          const id = image.path.split(/\/|\\/).slice(-2)[0];
-          const host = `http://${config.ip}:${config.clientPort}`;
-          const src = `${host}/static/${id}/${image.name}.${image.ext}`;
-          const thumbnailPath = image.noThumbnail
-            ? src
-            : `${host}/static/${id}/${image.name}_thumbnail.png`;
-
+        const image = child.images?.[0];
+        if (!image || !config) {
           return (
             <Card key={child.id}>
-              <Image
-                src={thumbnailPath}
-                className="absolute left-0 top-0 h-full w-full object-cover object-center transition-all ease-in-out"
-                layout="fill"
-              />
+              <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-neutral pb-14 transition-all ease-in-out">
+                <LightBulbIcon className="h-24 w-24 text-neutral-content" />
+              </div>
             </Card>
           );
-        })}
-      </div>
-    )
+        }
+
+        const id = image.path.split(/\/|\\/).slice(-2)[0];
+        const host = `http://${config.ip}:${config.clientPort}`;
+        const src = `${host}/static/${id}/${image.name}.${image.ext}`;
+        const thumbnailPath = image.noThumbnail
+          ? src
+          : `${host}/static/${id}/${image.name}_thumbnail.png`;
+
+        return (
+          <Card key={child.id}>
+            <Image
+              src={thumbnailPath}
+              className="absolute left-0 top-0 h-full w-full object-cover object-center transition-all ease-in-out"
+              layout="fill"
+            />
+          </Card>
+        );
+      })}
+    </div>
+  ) : (
+    <Error
+      statusCode={204}
+      statusMessage="Hey，这里什么都没有！"
+      btnText="返回首页"
+    />
   );
 };
 
